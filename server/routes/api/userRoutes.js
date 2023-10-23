2252a1f3cbef9c91e0f09696c095f2b5694eb246
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-23 06:08:52 -0300

    Added user routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,9 +1,18 @@
 const router = require('express').Router()
 
 const {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUsers,
+    getOneUser,
+    updateUser,
+    deleteUser
 } = require ('../../controllers/userController')
 
+router.route('/').get(getUsers).post(registerUser)
+router.route('/:userID').get(getOneUser).put(updateUser).delete(deleteUser)
 router.route('/auth').post(authUser)
+router.route('/logout').post(logoutUser)
 
 module.exports = router
