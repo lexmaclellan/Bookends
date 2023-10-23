b22fb4332b786b970efd79d4b5d714227a1a4459
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-23 03:14:09 -0300

    Included BookCardGrid

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { AiOutlineEdit } from 'react-icons/ai'
-import { BsInfoCircle } from 'react-icons/bs'
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+import { MdOutlineAddBox } from 'react-icons/md'
 import axios from 'axios'
 import Spinner from '../components/Spinner'
+import BookCardGrid from '../components/home/BookCardGrid'
 
 function Home() {
     const [books, setBooks] = useState([])
@@ -35,39 +34,7 @@ function Home() {
                 <Spinner />
             ) : (
                 <>
-                    {books.map((book, index) => (
-                        <div className='w-full border-separate border-spacing-2'>
-                            <div className='container inline-block'>
-                                <div key={book._id} className='row h-8'>
-                                    <div className='col border border-slate-700 rounded-md text-center'>
-                                        {index + 1}
-                                    </div>
-                                    <div className='col border border-slate-700 rounded-md text-center'>
-                                        {book.title}
-                                    </div>
-                                    <div className='col border border-slate-700 rounded-md text-center max-md:hidden'>
-                                        {book.author}
-                                    </div>
-                                    <div className='col border border-slate-700 rounded-md text-center max-md:hidden'>
-                                        {book.publishedYear}
-                                    </div>
-                                    <div className='col border border-slate-700 rounded-md text-center'>
-                                        <div className='flex justify-center gap-x-4'>
-                                            <Link to={`/books/details/${book._id}`}>
-                                                <BsInfoCircle className='text-2xl text-green-800' />
-                                            </Link>
-                                            <Link to={`/books/edit/${book._id}`}>
-                                                <AiOutlineEdit className='text-2xl text-yellow-600' />
-                                            </Link>
-                                            <Link to={`/books/delete/${book._id}`}>
-                                                <MdOutlineDelete className='text-2xl text-red-600' />
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                    <BookCardGrid books={books} />
                 </>
             )}
         </div>
