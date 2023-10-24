9c95d043ee00bf9e3c5cf17713c9415cf07ac0db
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-24 00:45:17 -0300

    Added AuthContextProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
-  </BrowserRouter>,
+  <AuthContextProvider>
+    <BrowserRouter>
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
+    </BrowserRouter>
+  </AuthContextProvider>
 )
