2ab09a42e1349fdeff58cfbd609d99f9d2c77fed
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-24 23:59:52 -0300

    Added dark styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,17 +2,18 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <header className='bg-gray-800'>
+    <header className='bg-slate-900 border-b-'>
         <div className='container flex w-full'>
             <div className='flex flex-col'>
-                <Link className='px-5 py-2 text-gray-50' to='/'>
+                <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/'>
                     <h1>Bookends</h1>
                 </Link>
             </div>
             <nav className='flex items-center'>
                 <div>
-                    <Link className='px-5 py-2 text-gray-50' to='/login'>Login</Link>
-                    <Link className='px-5 py-2 text-gray-50' to='/signup'>Signup</Link>
+                    <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/users'>Users</Link>
+                    <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/login'>Login</Link>
+                    <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/signup'>Signup</Link>
                 </div>
             </nav>
         </div>
