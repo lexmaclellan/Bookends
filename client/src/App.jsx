12c01efb32c08e1e344930d613041a06a296d9e1
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-10-24 23:58:22 -0300

    Added users list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import BooksList from './pages/BooksList'
 import CreateBook from './pages/CreateBooks'
 import ShowBook from './pages/ShowBook'
 import EditBook from './pages/EditBook'
@@ -8,13 +8,15 @@ import DeleteBook from './pages/DeleteBook'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
+import Users from './pages/UsersList'
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<BooksList />} />
+        <Route path='/users' element={<Users />} />
         <Route path='/books/create' element={<CreateBook />} />
         <Route path='/books/details/:bookID' element={<ShowBook />} />
         <Route path='/books/edit/:bookID' element={<EditBook />} />
