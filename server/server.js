795e868ff33c170f63791c65c430f89e2028f186
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-02 02:32:20 -0300

    Imports corsOptions

--- server/server.js
+++ server/server.js
@@ -4,23 +4,12 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const { notFound, errorHandler } = require('./middleware/errorHandler')
 const { logger, logEvents } = require('./middleware/logEvents')
+const corsOptions = require('./config/corsOptions')
 const db = require('./config/connection')
 const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001
 const app = express()
-
-const whitelist = ['http://127.0.0.1:5173', 'http://localhost:5173'];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
 
 app.use(logger)
 app.use(cors(corsOptions))
