4b42b4f7c962d9931c74c6ea3de36affd32b6ac5
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-02 02:16:02 -0300

    Added logger and corsOptions

--- server/server.js
+++ server/server.js
@@ -2,17 +2,31 @@ require('dotenv').config({ path: '../.env'} )
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const { notFound, errorHandler } = require('./middleware/error')
+const { notFound, errorHandler } = require('./middleware/errorHandler')
+const { logger, logEvents } = require('./middleware/logEvents')
 const db = require('./config/connection')
 const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
+const whitelist = ['http://127.0.0.1:5173', 'http://localhost:5173'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(logger)
+app.use(cors(corsOptions))
 app.unsubscribe(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors())
 app.use(routes)
 app.use(notFound)
 app.use(errorHandler)
