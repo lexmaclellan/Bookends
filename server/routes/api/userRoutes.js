d45c136d17783cf4bb61734bc84950e5a7e0ceea
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-02 02:43:34 -0300

    Added role routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -7,11 +7,14 @@ const {
     getUsers,
     getOneUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addRole,
+    removeRole
 } = require ('../../controllers/userController')
 
 router.route('/').get(getUsers).post(registerUser)
 router.route('/:userID').get(getOneUser).put(updateUser).delete(deleteUser)
+router.route('/:userID/roles').put(addRole).delete(removeRole)
 router.route('/auth').post(authUser)
 router.route('/logout').post(logoutUser)
 
