10d05325102b884be5263393e9edcae036efec71
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-06 22:57:35 -0400

    Added API server to whitelist

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,6 +1,8 @@
 const whitelist = [
     'http://127.0.0.1:5173',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://127.0.0.1:5000',
+    'http://localhost:5000'
 ]
 
 const corsOptions = {
@@ -11,6 +13,7 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    //credentials: true,
     optionsSuccessStatus: 200
 }
 
