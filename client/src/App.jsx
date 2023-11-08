30c2c716130a5fc6844ccd764d8d344854411205
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-08 01:37:09 -0400

    Added role permissions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,13 @@ import Layout from './components/Layout'
 import Missing from './components/Missing'
 import RequireAuth from './components/RequireAuth'
 
+const ROLES = {
+  'Admin': 6401,
+  'Editor': 8170,
+  'User': 3509,
+  'Banned': 4444
+}
+
 function App() {
   return (
     <main className='App'>
@@ -26,7 +33,7 @@ function App() {
           <Route path='/books/details/:bookID' element={<ShowBook />} />
           
           {/* protect these routes */}
-          <Route element={<RequireAuth />}>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
             <Route path='/admin' element={<Admin />} />
             <Route path='/books/create' element={<CreateBook />} />
             <Route path='/books/edit/:bookID' element={<EditBook />} />
