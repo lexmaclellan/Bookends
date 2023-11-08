dded73248aa358022ff501d183afd43f890ff310
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-07 21:57:18 -0400

    Restructured, started adding protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import BooksList from './pages/BooksList'
 import CreateBook from './pages/CreateBooks'
@@ -9,23 +8,34 @@ import ShowBook from './pages/ShowBook'
 import Signup from './pages/Signup'
 import Users from './pages/UsersList'
 import Navbar from './components/Navbar'
+import Layout from './components/Layout'
+import Missing from './components/Missing'
+import RequireAuth from './components/RequireAuth'
 
 function App() {
   return (
-    <>
+    <main className='App'>
       <Navbar />
       <Routes>
-        <Route path='/' element={<BooksList />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/users/account-recovery' element={<></>} />
-        <Route path='/books/create' element={<CreateBook />} />
-        <Route path='/books/details/:bookID' element={<ShowBook />} />
-        <Route path='/books/edit/:bookID' element={<EditBook />} />
-        <Route path='/books/delete/:bookID' element={<DeleteBook />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Layout />}>
+          {/* public routes */}
+          <Route path='/' element={<BooksList />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/users/account-recovery' element={<></>} />
+          <Route path='/books/details/:bookID' element={<ShowBook />} />
+          
+          {/* protect these routes */}
+          <Route path='/users' element={<Users />} />
+          <Route path='/books/create' element={<CreateBook />} />
+          <Route path='/books/edit/:bookID' element={<EditBook />} />
+          <Route path='/books/delete/:bookID' element={<DeleteBook />} />
+
+          {/* catch all */}
+          <Route path='*' element={<Missing />} />
+        </Route>
       </Routes>
-    </>
+    </main>
   )
 }
 
