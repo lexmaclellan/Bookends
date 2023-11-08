b468b4f05fa2d1471f007af9d895034e974689d1
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-08 00:07:16 -0400

    Moved allowed origins list to own file

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,19 +1,13 @@
-const whitelist = [
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
-    'http://127.0.0.1:5000',
-    'http://localhost:5000'
-]
+const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    //credentials: true,
     optionsSuccessStatus: 200
 }
 
