154a122eef0339fe86bc1cd0eb55ede2f501d02b
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-09 01:30:58 -0400

    Added dropdown icon for small screens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { IoLibrary } from 'react-icons/io5'
+import { FaBars } from 'react-icons/fa6'
 import { useAuth } from '../hooks/useAuth'
 import useLogout from '../hooks/useLogout'
 
 function Navbar() {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
     const { auth } = useAuth()
     const navigate = useNavigate()
     let location = useLocation()
@@ -15,6 +19,11 @@ function Navbar() {
         navigate('/login')
     }
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    /* check for user authentication on route change */
     useEffect(() => {
         if (auth.accessToken) {
             setIsAuthenticated(true)
@@ -23,35 +32,37 @@ function Navbar() {
             setIsAuthenticated(false)
         }
     }, [location])
-    
+
     return (
         <header className='shadow-md w-full'>
-            <div className='md:flex bg-slate-900 items-center justify-between py-4 md:px-10 px-7'>
+            <div className='flex bg-slate-900 items-center justify-between py-4 px-10'>
                 <div className='font-bold text-2xl flex items-center text-gray-50 '>
                     <Link className='hover:underline hover:text-sky-200 flex' to='/'>
                         <span className='text-3xl text-gray-50 mr-1 flex-col'>
-                            <ion-icon name="library"></ion-icon>
+                            <IoLibrary />
                         </span>
                         <h1 className='flex-col'>Bookends</h1>
                     </Link>
                 </div>
-                <ul className='md:flex md:items-center'>
-                    <li>
-                        <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/admin'>Admin</Link>
+                <ul className='md:flex md:items-center py-2'>
+                    <li className='md:flex hidden'>
+                        <Link className='px-5 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/admin'>Admin</Link>
                     </li>
                     {!isAuthenticated
                         ? <>
-                            <li>
-                                <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/login'>Login</Link>
+                            {/* Display login and signup links if user is not logged in */}
+                            <li className='md:flex hidden'>
+                                <Link className='px-5 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/login'>Login</Link>
                             </li>
-                            <li>
-                                <Link className='px-5 py-2 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/signup'>Signup</Link>
+                            <li className='md:flex hidden'>
+                                <Link className='px-5 text-gray-50 font-semibold hover:underline hover:text-sky-200' to='/signup'>Signup</Link>
                             </li>
                         </>
                         : <>
-                            <li>
+                            {/* Display logout button if user is logged in */}
+                            <li className='md:flex hidden'>
                                 <button
-                                    className='px-5 py-2 bg-transparent text-gray-50 font-semibold hover:underline hover:text-sky-200'
+                                    className='px-5 bg-transparent text-gray-50 font-semibold hover:underline hover:text-sky-200'
                                     onClick={signOut}
                                 >
                                     Logout
@@ -59,7 +70,9 @@ function Navbar() {
                             </li>
                         </>
                     }
-                    
+                    <li className='md:hidden'>
+                        <FaBars className='text-2xl' />
+                    </li>
                 </ul>
             </div>
         </header>
