6e2db89016d6d5b139e6c907dad3ceae55eb6ca2
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-13 22:20:47 -0400

    Initial commit

---
+++ client/src/components/Books/Rating.jsx
@@ -0,0 +1,40 @@
+import { FaStar, FaRegStarHalf, FaRegStar } from 'react-icons/fa'
+
+const Rating = ({ value, text }) => {
+    let hasValue
+    if (value)
+        hasValue = true
+    else
+        hasValue = false
+  return (
+    <>
+        {hasValue
+            ?
+                <section className='rating flex mb-2'>
+                    <span className='flex-col'>
+                        { value >= 1 ? <FaStar /> : value >= 0.5 ? <FaRegStarHalf /> : <FaRegStar /> }
+                    </span>
+                    <span className='flex-col'>
+                        { value >= 2 ? <FaStar /> : value >= 1.5 ? <FaRegStarHalf /> : <FaRegStar /> }
+                    </span>
+                    <span className='flex-col'>
+                        { value >= 3 ? <FaStar /> : value >= 2.5 ? <FaRegStarHalf /> : <FaRegStar /> }
+                    </span>
+                    <span className='flex-col'>
+                        { value >= 4 ? <FaStar /> : value >= 3.5 ? <FaRegStarHalf /> : <FaRegStar /> }
+                    </span>
+                    <span className='flex-col'>
+                        { value >= 5 ? <FaStar /> : value >= 4.5 ? <FaRegStarHalf /> : <FaRegStar /> }
+                    </span>
+                    <span className='rating-text flex-col'>{ text && `${text} Reviews` }</span>
+                </section>
+            :
+                <section className='rating mb-2'>
+                </section>
+        }
+    </>
+  )
+}
+
+
+export default Rating
