bfcb4e220e03503fbff4c0b6fb9e720dfc0a430c
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-15 01:55:25 -0400

    Initial commit

---
+++ client/src/constants.jsx
@@ -0,0 +1,5 @@
+export const BASE_URL = ''
+export const BOOKS_URL = '/api/books'
+export const USERS_URL = '/api/users'
+export const ORDERS_URL = '/api/orders'
+export const STRIPE_URL = '/api/config/stripe'
