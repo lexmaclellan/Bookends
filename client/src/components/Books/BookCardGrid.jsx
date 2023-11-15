575eec2acbf2ede10e96772211f279c18bc7b544
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-15 01:18:19 -0400

    Fixed a list key error

--- client/src/components/Books/BookCardGrid.jsx
+++ client/src/components/Books/BookCardGrid.jsx
@@ -5,7 +5,7 @@ function BookCard({ books }) {
   return (
     <Row>
       {books.map((book) => (
-        <Col sm={12} md={6} lg={4} xl={3}>
+        <Col key={book._id} sm={12} md={6} lg={4} xl={3}>
           <BookCardSingle key={book._id} book={book} />
         </Col>
       ))}
