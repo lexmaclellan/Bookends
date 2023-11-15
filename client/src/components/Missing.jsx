0925a45a001ef5bb970b97c7c7ac56a863ba36dc
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-15 00:51:57 -0400

    Changed colors

--- client/src/components/Missing.jsx
+++ client/src/components/Missing.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 function Missing() {
   return (
-    <article className='p-12'>
-        <h1>404 Page Not Found</h1>
-        <Link to='/'>Return Home</Link>
+    <article className='p-12 text-stone-700'>
+        <h1 className='font-semibold text-2xl mb-2'>404 Page Not Found</h1>
+        <Link to='/' className='font-semibold underline hover:no-underline'>Return Home</Link>
     </article>
   )
 }
