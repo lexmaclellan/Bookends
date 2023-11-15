6cf485fa8d81f94f8c107abc18f03ee9c68d4db0
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-15 00:52:12 -0400

    Added redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,17 +5,21 @@ import './assets/styles/index.css'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store.jsx'
 import { SnackbarProvider } from 'notistack'
 import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthContextProvider>
-      <SnackbarProvider>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-      </SnackbarProvider>
+      <Provider store={store}>
+        <SnackbarProvider>
+          <Routes>
+            <Route path='/*' element={<App />} />
+          </Routes>
+        </SnackbarProvider>
+      </Provider>
     </AuthContextProvider>
   </BrowserRouter>
 )
