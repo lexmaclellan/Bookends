d0b6ef7dc78d4e5cc1f9849b4520043863055ac2
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-16 17:23:58 -0400

    Changed user data

--- server/data/users.js
+++ server/data/users.js
@@ -4,7 +4,7 @@ const users = [
     {
         name: 'Lex MacLellan',
         email: 'lex@email.com',
-        password: bcrypt.hashSync('supersecret420', 10),
+        password: bcrypt.hashSync('supersecret', 10),
         roles: {
             User: 3509,
             Admin: 6401
@@ -13,18 +13,9 @@ const users = [
     {
         name: 'Jane Doe',
         email: 'jane@email.com',
-        password: bcrypt.hashSync('swordfish', 10),
-        roles: {
-            User: 3509
-        }
-    },
-    {
-        name: 'Joe Schmoe',
-        email: 'joe@email.com',
         password: bcrypt.hashSync('12345678', 10),
         roles: {
-            User: 3509,
-            Banned: 4444
+            User: 3509
         }
     }
 ]
