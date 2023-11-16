bf479e84e1ad4aa79df8e3e461a254f2deabc61e
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-15 21:18:35 -0400

    Added user profile routes

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -6,8 +6,10 @@ const verifyJWT = require('../../middleware/verifyJWT')
 const {
     authUser,
     registerUser,
-    logoutUser,
     handleRefreshToken,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
     getUsers,
     getOneUser,
     updateUser,
@@ -18,8 +20,9 @@ const {
 
 router.route('/').get(getUsers).post(registerUser)
 router.route('/auth').post(authUser)
+router.route('/profile').get(getUserProfile).put(updateUserProfile)
 router.route('/refresh').get(handleRefreshToken)
-router.route('/logout').get(logoutUser)
+router.route('/logout').post(logoutUser)
 router.route('/:userID').get(getOneUser).put(updateUser).delete(deleteUser)
 router.route('/:userID/roles').put(addRole).delete(removeRole)
 
