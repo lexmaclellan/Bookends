429649adf117d9bfb30f48fd56578ba3767f4b6e
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-16 03:11:50 -0400

    Added saveShippingAddress reducer

--- client/src/slices/cartSlice.jsx
+++ client/src/slices/cartSlice.jsx
@@ -3,7 +3,7 @@ import { updateCart } from '../utils/cartUtils'
 
 const initialState = localStorage.getItem('cart')
     ? JSON.parse(localStorage.getItem('cart'))
-    : { cartItems: [], shippingAddress: {}, paymentMethod: '' }
+    : { cartItems: [], shippingAddress: {}, paymentMethod: 'Stripe' }
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -24,9 +24,13 @@ const cartSlice = createSlice({
         removeFromCart: (state, action) => {
             state.cartItems = state.cartItems.filter((i) => i._id !== action.payload)
             return updateCart(state)
+        },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload
+            return updateCart(state)
         }
     }
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions
 export default cartSlice.reducer
