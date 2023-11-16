eb4611615947aa5293e2037b7c755844d2fd8483
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-15 23:35:21 -0400

    Added user profile, update and delete routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -114,14 +114,53 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route    GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile')
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            roles: user.roles,
+            refreshToken: user.refreshToken,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
+        })
+    } else {
+        res.status(404)
+        throw new Error('No user found with that ID.')
+    }
 })
 
 // @desc    Update user profile
 // route    PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('update user profile')
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            roles: user.roles,
+            refreshToken: user.refreshToken,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
+        })
+    } else {
+        res.status(404)
+        throw new Error('No user found with that ID.')
+    }
 })
 
 // @desc    Get all users
@@ -156,8 +195,7 @@ const getOneUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.userID)
 
     if (user) {
-        const userList = []
-        userList.push({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -166,7 +204,6 @@ const getOneUser = asyncHandler(async (req, res) => {
             createdAt: user.createdAt,
             updatedAt: user.updatedAt
         })
-        res.status(200).json(userList)
     } else {
         res.status(404)
         throw new Error('No user found with that ID.')
@@ -177,14 +214,45 @@ const getOneUser = asyncHandler(async (req, res) => {
 // route    PUT /api/users/profile
 // @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Update User' })
+    const user = await User.findById(req.params.userID)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            roles: user.roles,
+            refreshToken: user.refreshToken,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
+        })
+    } else {
+        res.status(404)
+        throw new Error('No user found with that ID.')
+    }
 })
 
 // @desc    Delete user
 // route    DELETE /api/users/:userID
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Delete User' })
+    const user = await User.findByIdAndDelete(req.params.userID)
+
+    if (user) {
+        res.status(200).json({ message: 'User deleted.' })
+    } else {
+        res.status(404)
+        throw new Error('No user found with that ID.')
+    }
 })
 
 // @desc    Add role to user
@@ -209,7 +277,18 @@ const addRole = asyncHandler(async (req, res) => {
 // route    PUT /api/users/:userID/roles
 // @access  Private/Admin
 const removeRole = asyncHandler(async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.params.userID,
+        { $pull: { roles: req.body } },
+        { new: true }
+    )
 
+    if (user) {
+        res.status(200).json({ message: 'Role removed successfully.' })
+    } else {
+        res.status(404)
+        throw new Error('No user found with that ID.')
+    }
 })
 
 module.exports = {
