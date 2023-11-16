3c8b8fc6690c8e3576d13c49c1a5f88cdf8618f6
lexrayne <30938032+lexrayne@users.noreply.github.com>
2023-11-16 01:57:40 -0400

    Added logout mutation

--- client/src/slices/usersApiSlice.jsx
+++ client/src/slices/usersApiSlice.jsx
@@ -5,13 +5,19 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: USERS_URL/auth,
+                url: `${USERS_URL}/auth`,
                 method: 'POST',
                 body: data
             }),
             keepUnusedDataFor: 5
+        }),
+        logout: builder.mutation({
+            query: () => ({
+                url: `${USERS_URL}/logout`,
+                method: 'POST'
+            })
         })
     })
 })
 
-export const { useLoginMutation } = usersApiSlice
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice
